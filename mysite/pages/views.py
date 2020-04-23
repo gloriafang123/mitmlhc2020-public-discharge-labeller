bebaf77154e80e035d4ff73bb57d14a541d5d5e5
Gloria Fang <gloria@Glorias-MacBook-Pro.local>
2020-04-23 18:02:14 -0400

    template tags

--- mysite/pages/views.py
+++ mysite/pages/views.py
@@ -8,5 +8,13 @@ def home_view(r, *args, **kwargs): #first argument required.
 	return HttpResponse("<h1> Hello world</h1>")
 
 def other_view(r, *args, **kwargs):
+	context = {'variable': 123, 
+	'variable2': "variable2",
+	'listitem': [1,2,3]}
+
+	# try to change if statements in view (context), not so much logic in render
+
 	print("request", r, args, kwargs) #r is a WSGI request, can check request.user etc info
-	return render(r, "other_view.html", {'variable': 123, 'variable2': "variable2"}) #knows this from templates.
+	return render(r, "other_view.html", 
+	context) #knows this from templates.
+	# can go into any of the templates rendered, not have to be other_view, can be include.html also
